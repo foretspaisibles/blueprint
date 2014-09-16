9dfb3c6cbaab4e722acf5f29894dbdccfa753b01
Michael Grünewald <michipili@gmail.com>
2014-09-16 07:43:08 +0200

    Prepare a build system

---
+++ Library/Make/blueprint.subdir.mk
@@ -0,0 +1,19 @@
+### blueprint.subdir.mk -- Delegate task to subdirectories
+
+# Author: Michael Grünewald
+# Date: Sun Sep 14 23:57:54 CEST 2014
+
+# Blueprint (https://bitbucket.org/michipili/blueprint)
+# This file is part of Blueprint
+#
+# Copyright © 2014 Michael Grünewald
+#
+# This file must be used under the terms of the CeCILL-B.
+# This source file is licensed as described in the file COPYING, which
+# you should have received as part of this distribution. The terms
+# are also available at
+# http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.txt
+
+.include "bps.subdir.mk"
+
+### End of file `blueprint.subdir.mk'
