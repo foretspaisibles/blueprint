ff39304651b8563c9488e5102e0c2bb2638c56a6
Michael Grünewald <michipili@gmail.com>
2015-01-26 00:15:59 +0100

    Use ML-signals instead of the subject-observer pattern

--- chart-ocaml/chart.ml
+++ chart-ocaml/chart.ml
@@ -21,6 +21,7 @@ let series2 = [| 28.; 48.; 40.; 19.; 86.; 27.; 90.; |]
 (* Canvas properties *)
 module CanvasProperties =
 struct
+
   let canvas_properties_scale_unit =
     4.0
   type t = {
@@ -28,91 +29,89 @@ struct
     scale: float;
   }
 
-class ['observer] subject =
-object(self)
-  inherit ['observer] PatternSubjectObserver.subject
-  val mutable bg = `WHITE
-  val mutable scale = 1.0
-  method get = {
-    bg;
-    scale;
-  }
-  method notify_change =
-    self#notify (fun obs -> obs#canvas_properties_changed)
-  method set props =
-    bg <- props.bg;
-    scale <- props.scale;
-    self#notify_change
-  method set_bg x =
-    bg <- x;
-    self#notify_change
-  method set_scale x =
-    scale <- x;
-    self#notify_change
-end
-
-class virtual ['subject] observer s =
-object(self : 'selftype)
-  inherit ['subject] PatternSubjectObserver.observer
-  val mutable subject = s
-  method virtual canvas_properties_changed : 'selftype subject -> unit
-  method set_canvas_properties (s :  'selftype subject) =
-    subject <- s;
-    subject#add self;
-    self#canvas_properties_changed subject
-end
-
-class ['subject] user canvas s =
-object(self)
-  inherit ['subject] observer s
-  method canvas_properties_changed s =
-    let canvas_properties = s#get in
-    canvas#set_pixels_per_unit
-	     (canvas_properties_scale_unit *. canvas_properties.scale);
-    canvas#misc#modify_bg [`NORMAL, canvas_properties.bg];
-end
-
-class ['subject] editor s =
-  let canvas_properties = s#get in
-  let container = GPack.hbox () in
-  let scale =
-    GData.adjustment
-      ~value:canvas_properties.scale
-      ~lower:0.01
-      ~upper:8.0
-      ~step_incr:0.01
-      ~page_incr:0.1
-      ~page_size:0.0
-      ()
-  in
-  let scaleselect =
-    GRange.scale
-      `HORIZONTAL
-      ~adjustment:scale
-      ~digits:2
-      ~packing:container#add () in
-  let bgselect =
-    GButton.color_button
-      ~color:(GDraw.color canvas_properties.bg)
-      ~packing:container#add () in
-  object(self)
-  inherit GObj.widget container#as_widget
-  inherit ['subject] observer s
-  method canvas_properties_changed s =
-    let canvas_properties = s#get in
-    scale#set_value canvas_properties.scale;
-    bgselect#set_color (GDraw.color canvas_properties.bg);
-  method private callback_changed () =
-    let canvas_properties = {
-      scale = scale#value;
-      bg = (`COLOR bgselect#color);
+  class subject =
+    let defaults = {
+      bg = `WHITE;
+      scale = 1.0;
     } in
-    subject#set canvas_properties
-  initializer
-    ignore [
-	scale#connect#value_changed ~callback:self#callback_changed;
-	bgselect#connect#color_set ~callback:self#callback_changed;
-      ]
+    object(self)
+      inherit [t] GUtil.variable defaults
+      method set_bg x =
+	self#set { self#get with bg = x }
+      method set_scale x =
+	self#set { self#get with scale = x }
+    end
+
+  class virtual observer =
+    object(self)
+      val mutable subject = None
+      val mutable signalids = []
+      method virtual canvas_properties_changed : t -> unit
+      method set_canvas_properties (props : subject) =
+	Gaux.may
+	  (fun oldprops -> List.iter oldprops#connect#disconnect signalids)
+	  subject;
+	subject <- Some(props);
+	signalids <- [
+	  props#connect#set
+			    ~callback:self#canvas_properties_changed
+	];
+	self#canvas_properties_changed props#get
+    end
+
+  class user canvas canvas_properties =
+  object(self)
+    inherit observer
+    method canvas_properties_changed props =
+      canvas#set_pixels_per_unit
+	       (canvas_properties_scale_unit *. props.scale);
+      canvas#misc#modify_bg [`NORMAL, props.bg];
+    initializer
+      self#set_canvas_properties canvas_properties
+  end
+
+  class editor props =
+    let canvas_properties = props#get in
+    let container = GPack.hbox () in
+    let scale =
+      GData.adjustment
+	~value:canvas_properties.scale
+	~lower:0.01
+	~upper:8.0
+	~step_incr:0.01
+	~page_incr:0.1
+	~page_size:0.0
+	()
+    in
+    let scaleselect =
+      GRange.scale
+	`HORIZONTAL
+	~adjustment:scale
+	~digits:2
+	~packing:container#add () in
+    let bgselect =
+      GButton.color_button
+	~color:(GDraw.color canvas_properties.bg)
+	~packing:container#add () in
+    object(self)
+      inherit GObj.widget container#as_widget
+      inherit observer
+
+      method canvas_properties_changed props =
+	scale#set_value props.scale;
+	bgselect#set_color (GDraw.color props.bg);
+      method private callback_changed () =
+	let canvas_properties = {
+	  scale = scale#value;
+	  bg = (`COLOR bgselect#color);
+	} in
+	Gaux.may (fun props -> props#set canvas_properties) subject
+      initializer
+	self#set_canvas_properties props;
+	ignore [
+	    scale#connect#value_changed ~callback:self#callback_changed;
+	    bgselect#connect#color_set ~callback:self#callback_changed;
+	  ];
   end
 
   let editor
@@ -163,7 +162,7 @@ class ['subject] chart () =
   in
   object
     inherit GObj.widget vbox#as_widget
-    inherit ['subject] CanvasProperties.user canvas canvas_properties
+    inherit CanvasProperties.user canvas canvas_properties
     initializer
       canvas#set_pixels_per_unit 5.0;
   end
