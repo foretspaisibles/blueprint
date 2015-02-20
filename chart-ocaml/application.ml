ef73ece168c0de1fa766e5ac98556bcb331eddf9
Michael Grünewald <michipili@gmail.com>
2015-02-20 22:41:19 +0100

    Prune cards

--- chart-ocaml/application.ml
+++ chart-ocaml/application.ml
@@ -16,18 +16,6 @@ http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.txt *)
 open GMain
 open GdkKeysyms
 
-let card1 = {
-  Card.
-  name = "Lucy";
-  surname = "Wadham";
-}
-
-let card2 = {
-  Card.
-  name = "Rebecca";
-  surname = "West";
-}
-
 let mainwindow () =
   let window = GWindow.window ~width:320 ~height:240 ~title:"Test OCaml charts" () in
   let vbox = GPack.vbox ~packing:window#add () in
@@ -47,26 +35,13 @@ let mainwindow () =
       ~packing:vbox#add
       ()
   in
-  (* Models *)
-  let model1 = new Card.model card1 in
-  let model2 = new Card.model card2 in
-
-  (* Cards *)
-  let viewcard1 = Card.presenter ~packing:vbox#add ~model:model1 () in
-  let viewcard2 = Card.presenter ~packing:vbox#add ~model:model2 () in
-  let editcard1 = Card.edit ~packing:vbox#add ~model:model1 () in
-  let editcard2 = Card.edit ~packing:vbox#add ~model:model2 () in
 
   let _ = factory#add_item "Connect" ~key:_C
-			   ~callback:(fun () -> viewcard2#set_model model1)
+			   ~callback:(fun () -> print_endline "Connect!")
   in
 
   (* Display the windows and enter Gtk+ main loop *)
   let _ =   window#connect#destroy ~callback:Main.quit in
-  viewcard1#misc#hide ();
-  editcard1#misc#hide ();
-  viewcard2#misc#hide ();
-  editcard2#misc#hide ();
   window#add_accel_group accel_group;
   window#show ();
   window
